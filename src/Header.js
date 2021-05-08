38e535b9dbbb6c2a9b5e4ebc89790b31b072ffca
sai darshan <saidarshan27@gmail.com>
2021-05-08 18:07:15 +0530

    add header with inline styles

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <header style={{textAlign:"center",backgroundColor:"black",textTransform:"uppercase",color:"white",padding:20}}>
+            Phone Directory
+        </header>
+    )
+}
+
+export default Header;
