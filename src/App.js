5c95567bfc18e6530e08d070519d5c8c8408b57a
sai darshan <saidarshan27@gmail.com>
2021-05-09 00:39:17 +0530

    add common folder with common stylesheet

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
+import React,{Component} from "react";
 import Header from "./Header";
 import "./App.css"
-function App() {
-  let persons = [
-    {
-      id:1,
-      name:"Sai Darshan",
-      phone:"8951426231"
-    },
-    {
-      id:2,
-      name:"Doyle",
-      phone:"7019879155"
-    }
-  ]
 
-  function deleteHandler(){
-    alert("delete clicked");
-  }
-  return (
-    <div>
+class App extends Component{
+  render(){
+    let persons = [
+      {
+        id:1,
+        name:"Sai Darshan",
+        phone:"8951426231"
+      },
+      {
+        id:2,
+        name:"Doyle",
+        phone:"7019879155"
+      }
+    ];
+
+    function deleteHandler(){
+      alert("delete clicked");
+    };
+
+    return(
+      <div>
       <Header headerName="Phone Directory"/>
       <div className="wrapper">
-        <button className="add-btn">ADD</button>
+        <button className="btn add-btn">ADD</button>
         <div className="grid-container">
           <span className="grid-item name-span heading">NAME</span>
           <span className="grid-item phone-span heading">PHONE</span>
@@ -31,13 +35,15 @@ function App() {
             return <div key={person.id} className="grid-container">
               <span className="grid-item name-span">{person.name}</span>
               <span className="grid-item phone-span">{person.phone}</span>
-              <button className="delete-btn" onClick={deleteHandler}>DELETE</button>
+              <button className="btn delete-btn" onClick={deleteHandler}>DELETE</button>
             </div>
           })
         }
       </div>
     </div>
-  );
+    )
+  }
 }
+
 
 export default App;
