9bae78d732b8239d10c0fcd6fdb68bbf7d79025a
sai darshan <saidarshan27@gmail.com>
2021-05-08 18:44:27 +0530

    add content dynamically

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import Header from "./Header";
 import "./App.css"
 function App() {
+  let persons = [
+    {
+      id:1,
+      name:"Sai Darshan",
+      phone:"8951426231"
+    },
+    {
+      id:2,
+      name:"Doyle",
+      phone:"7019879155"
+    }
+  ]
   return (
     <div>
       <Header/>
       <div className="wrapper">
         <button className="add-btn">ADD</button>
         <div className="grid-container">
-          <span className="grid-item name-span">NAME</span>
-          <span className="grid-item phone-span">PHONE</span>
+          <span className="grid-item name-span heading">NAME</span>
+          <span className="grid-item phone-span heading">PHONE</span>
         </div>
+        {
+          persons.map(person=>{
+            return <div key={person.id} className="grid-container">
+              <span className="grid-item name-span">{person.name}</span>
+              <span className="grid-item phone-span">{person.phone}</span>
+            </div>
+          })
+        }
       </div>
     </div>
   );
