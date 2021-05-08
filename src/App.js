cfdbf42b2fd7ba8f0658048923ad12879432d65a
sai darshan <saidarshan27@gmail.com>
2021-05-08 18:31:01 +0530

    add grid

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
       <Header/>
       <div className="wrapper">
         <button className="add-btn">ADD</button>
+        <div className="grid-container">
+          <span className="grid-item name-span">NAME</span>
+          <span className="grid-item phone-span">PHONE</span>
+        </div>
       </div>
     </div>
   );
