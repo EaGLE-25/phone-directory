cdaeb8b6aa9b995aff6e202010bcc046c838c38c
sai darshan <saidarshan27@gmail.com>
2021-05-09 01:42:51 +0530

    add state and onchange event for addSubsriber

--- src/AddSubscriber.js
+++ src/AddSubscriber.js
@@ -4,6 +4,20 @@ import "./AddSubscriber.css";
 
 
 class AddSubscriber extends Component{
+    constructor(){
+        super();
+        this.state = {
+            name:"",
+            phone:""
+        }
+    };
+
+    changeHandler = (e)=>{
+        const state = this.state;
+        state[e.target.name] = e.target.value;
+        this.setState(state);
+    }
+
     render(){
         return(
             <div>
@@ -13,22 +27,23 @@ class AddSubscriber extends Component{
                 <form className="add-subscriber-form"> 
                     <div className="control-container">
                         <label htmlFor="name" className="heading">Name</label>
-                        <input className="input-control" name="name"></input>
+                        <input className="input-control" name="name" onChange={this.changeHandler}></input>
                     </div>
                     <div className="control-container">
                         <label htmlFor="phone" className="heading">Phone</label>
-                        <input className="input-control" name="phone"></input>
+                        <input className="input-control" name="phone" onChange={this.changeHandler}></input>
                     </div>
                     <div className="subscriber-to-be-added">
                         <label>Subscriber to be added</label>
                         <div className="preview">
                             <div className="name-preview">
-                                <label className="heading">Name: </label>
+                                <label className="heading">Name: {this.state.name}</label>
                             </div>  
                             <div className="phone-preview">
-                                <label className="heading">Phone: </label>
+                                <label className="heading">Phone: {this.state.phone}</label>
                             </div>
                         </div>
+                        <button className="btn add-btn">ADD</button>
                     </div>
                 </form>
             </div>
